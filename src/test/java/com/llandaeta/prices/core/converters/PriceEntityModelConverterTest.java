f6d9c914cb4b621014f9b6450f2c5c06d6174fa5
lalandaeta <lalandaeta@avansis.es>
2021-04-27 02:03:00 +0200

    test

---
+++ src/test/java/com/llandaeta/prices/core/converters/PriceEntityModelConverterTest.java
@@ -0,0 +1,17 @@
+package com.llandaeta.prices.core.converters;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceEntityModelConverterTest{
+
+    @BeforeEach
+    void setUp(){
+    }
+
+    @Test
+    void convert(){
+    }
+}
---
+++ src/test/java/com/llandaeta/prices/core/services/impl/PriceServiceImplTest.java
@@ -0,0 +1,6 @@
+package com.llandaeta.prices.core.services.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PriceServiceImplTest {
+  
+}
